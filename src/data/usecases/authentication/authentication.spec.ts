14b7ed7d1a1174f98b0f0ebbe12df3e4026a6df8
Junior Trojilio <osmar.trojilio@gmail.com>
2022-03-16 02:01:46 -0300

    test: authentication calls http post client with correct body

--- src/data/usecases/authentication/authentication.spec.ts
+++ src/data/usecases/authentication/authentication.spec.ts
@@ -1,3 +1,4 @@
+import { AuthenticationParams } from '../../../domain/usecases/authentication'
 import HttpPostClientSpy from '../../mocks/httpPostClientSpy'
 import Authentication from './authentication'
 
@@ -15,11 +16,25 @@ const makeSut = (url = 'any_url'): SutTypes => {
 	}
 }
 
+const mockAuthenticationBody = (): AuthenticationParams => {
+	return {
+		email: 'any_email',
+		password: 'any_password',
+	}
+}
+
 describe('Authentication', () => {
-	test('should call http clientwith correct URL', () => {
+	test('should call http client with correct URL', async () => {
 		const url = 'other_url'
 		const { httpPostClientSpy, authenticationSut } = makeSut(url)
-		authenticationSut.auth()
+		await authenticationSut.auth(mockAuthenticationBody())
 		expect(httpPostClientSpy.url).toBe(url)
+	})
+
+	test('should call http client with correct body', async () => {
+		const { httpPostClientSpy, authenticationSut } = makeSut()
+		const mockedBody = mockAuthenticationBody()
+		await authenticationSut.auth(mockedBody)
+		expect(httpPostClientSpy.body).toEqual(mockedBody)
 	})
 })
