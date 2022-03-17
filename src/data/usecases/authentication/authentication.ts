7cfa1b2da9b748f891e9539fa5551d1082a1123e
Junior Trojilio <osmar.trojilio@gmail.com>
2022-03-17 11:25:48 -0300

    feat: implements authentication interface

--- src/data/usecases/authentication/authentication.ts
+++ src/data/usecases/authentication/authentication.ts
@@ -1,11 +1,11 @@
 import { AccountModel } from '../../../domain/models/accountModel'
 import InvalidCredentialsError from '../../../domain/errors/invalidCredentialsError'
 import UnexpectedError from '../../../domain/errors/unexpectedError'
-import { AuthenticationParams } from '../../../domain/usecases/authentication'
+import { AuthenticationParams } from '../../../domain/usecases/authentication/authentication'
 import { HttpPostClient } from '../../protocols/http/httpPostClient'
 import { HttpStatusCode } from '../../protocols/http/httpResponseClient'
 
-export default class Authentication {
+export default class Authentication implements Authentication {
 	constructor(
 		private readonly url: string,
 		private readonly httpPostClient: HttpPostClient<
@@ -17,7 +17,7 @@ export default class Authentication {
 		this.httpPostClient = httpPostClient
 	}
 
-	async auth({ email, password }: AuthenticationParams): Promise<void> {
+	async auth({ email, password }: AuthenticationParams): Promise<AccountModel> {
 		const httpResponse = await this.httpPostClient.post({
 			url: this.url,
 			body: { email, password },
@@ -25,7 +25,7 @@ export default class Authentication {
 
 		switch (httpResponse.statusCode) {
 			case HttpStatusCode.OK:
-				break
+				return httpResponse.body
 			case HttpStatusCode.UNAUTHORIZED:
 				throw new InvalidCredentialsError()
 			default:
